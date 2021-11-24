465591a6890f7ffb62891accb03b9aba501c8661
Obaida <o.alnakdali@gmail.com>
2021-11-24 11:08:54 +0200

    complete step 3 and 4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+name = prompt("Enter your name: ");
+surname = prompt("Enter your surname: ");
+city = prompt("Enter your city: ");
+alert("Name: " + name + "\nSurname: " + surname + "\nCity: " + city);
